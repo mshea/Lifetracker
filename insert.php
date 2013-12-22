425103a1573397cfe3ef047494bc919ece07c384
Michael E. Shea <mike@mikeshea.net>
2013-12-22 12:58:55 -0500

    tags -> key value pairs code added
    Added code to parse and dump a csv of key:value pairs stored in tags.
    This way, if you want to track something, you can type “key:value” into
    the tags and it will dump it out to csv along with the full data. This
    way you can say things like “steps:10434” and track it over the year.

--- insert.php
+++ insert.php
@@ -26,24 +26,44 @@ $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 try {
 $STH = $DBH->prepare("INSERT OR REPLACE INTO LIFEDATA (DATE, DATETIME, CREATE_SCORE, RELAX_SCORE, LOVE_SCORE, BEFRIEND_SCORE, HEALTH_SCORE, HAPPINESS_SCORE, TAGS, LATITUDE, LONGITUDE) values (:DATE, :DATETIME, :CREATE_SCORE, :RELAX_SCORE, :LOVE_SCORE, :BEFRIEND_SCORE, :HEALTH_SCORE, :HAPPINESS_SCORE, :TAGS, :LATITUDE, :LONGITUDE)");
-echo "<p>Submitted!";
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
 
 $STH->execute($data);
 
+## Dump lifedata to csv and keyvalues into keyvalues csv
+
 $results = $DBH->query("SELECT * FROM LIFEDATA");
 
+# Create csv file for data output
 $handle = fopen("lifedata.csv", "w");
-
 fputcsv($handle, array("Date", "DateTime", "Create", "Relax", "Love", "Befriend", "Health", "Happiness", "Tags", "Latitude", "Longitude"));
 
-foreach($results as $row)
-{
-fputcsv($handle, array($row['DATE'], $row['DATETIME'], $row['CREATE_SCORE'], $row['RELAX_SCORE'], $row['LOVE_SCORE'], $row['BEFRIEND_SCORE'], $row['HEALTH_SCORE'], $row['HAPPINESS_SCORE'], $row['TAGS'], $row['LATITUDE'], $row['LONGITUDE']));
+# Create csv file for key vlaue pairs
+$keyvaluehandle = fopen("lifedata_key_value_pairs.csv", "w");
+fputcsv($keyvaluehandle, array("Date", "Key", "Value"));
+
+# iterate over rows and dump to csvs
+foreach($results as $row) {
+	fputcsv($handle, array($row['DATE'], $row['DATETIME'], $row['CREATE_SCORE'], $row['RELAX_SCORE'], $row['LOVE_SCORE'], $row['BEFRIEND_SCORE'], $row['HEALTH_SCORE'], $row['HAPPINESS_SCORE'], $row['TAGS'], $row['LATITUDE'], $row['LONGITUDE']));
+	
+	# Split tags up into key-value pairs and dump to csv
+	$tags = explode(" ",$row['TAGS']);
+	if (is_array($tags)){
+		foreach($tags as $tag) {
+			if (strpos($tag,':') !== false) {
+				list($key,$value) = split(":",$tag);
+				fputcsv($keyvaluehandle, array($row['DATE'],$key,$value));
+			}
+		}
+	}
 }
-fclose($handle)
+fclose($handle);
+fclose($keyvaluehandle);
+
+echo "Submitted!";
+
 ?>
 </div><!-- /content -->
 </div><!-- /page -->
